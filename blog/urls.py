b640db452141afcf3797de2c3d7449396490d8b3
Jesse <rchikerema@gmail.com>
2025-11-04 18:32:10 +1300

    just a commit.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
+from blog.views import LoginView, LogoutView
 from blog.viewsets import CategoryViewSet, PostViewSet, ProfileViewSet, CommentViewSet
 
 router = DefaultRouter()
@@ -12,3 +14,8 @@ router.register(r'profiles', ProfileViewSet)
 router.register(r'comments', CommentViewSet)
 
 urlpatterns= router.urls
+
+urlpatterns += [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
